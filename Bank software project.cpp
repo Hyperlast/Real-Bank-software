62af3fb6e16cf48b61562fb380fbea3e2c42acfa
Hyperlast <72891329+Hyperlast@users.noreply.github.com>
2020-12-27 22:25:59 +0300

    Add files via upload

---
+++ Bank software project.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+void MainScreen()
+{
+    cout << "Although";
+}
+int main()
+{
+    cout << MainScreen;
+    return 0;
+}
+
